e5d1bd8c1a1caba8865995e9e65827948ab52821
TonnyKamau <tonnykamau6@gmail.com>
2021-08-07 18:56:39 +0300

    timedone

--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -9,7 +9,8 @@ class AddTasks extends StatefulWidget {
 }
 
 class _AddTasksState extends State<AddTasks> {
-   int _currentValue = 3;
+  int _hour = 1;
+  int _minutes = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,13 +28,13 @@ class _AddTasksState extends State<AddTasks> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 15.0, 0.0, 15.0),
+            padding: const EdgeInsets.fromLTRB(20.0, 10.0, 0.0, 15.0),
             child: Text('Task Name'),
           ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            padding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 15.0),
             child: Material(
-              elevation: 20.0,
+              elevation: 10.0,
               borderRadius: BorderRadius.only(
                   bottomLeft: Radius.circular(10.0),
                   bottomRight: Radius.circular(10.0)),
@@ -48,27 +49,84 @@ class _AddTasksState extends State<AddTasks> {
           Padding(
             padding: const EdgeInsets.fromLTRB(20.0, 15.0, 0.0, 15.0),
             child: Text('Time'),
-            
           ),
-           Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 15.0, 0.0, 15.0),
-            child: Row(children: [
-              SizedBox(
-                width: 100,
-                height: 100,
-                child: Card(
-                  color: Colors.orange[600],
-                  child: NumberPicker(
-                        value: _currentValue,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 90,
+                  height: 100,
+                  child: Center(
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.orange[600],
+                      child: NumberPicker(
+                        value: _hour,
+                        textStyle: TextStyle(fontSize: 20, color: Colors.white),
+                        selectedTextStyle: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
                         minValue: 1,
                         maxValue: 12,
-                        
-                        onChanged: (value) => setState(() => _currentValue = value),
+                        haptics: true,
+                        infiniteLoop: true,
+                        itemWidth: 100,
+                        itemHeight: 30,
+                        onChanged: (value) => setState(() => _hour = value),
                       ),
+                    ),
+                  ),
                 ),
-              ),
-                  
-            ],
+                SizedBox(
+                  width: 90,
+                  height: 100,
+                  child: Center(
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.orange[600],
+                      child: NumberPicker(
+                        value: _minutes,
+                        textStyle: TextStyle(fontSize: 20, color: Colors.white),
+                        selectedTextStyle: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                        minValue: 1,
+                        maxValue: 60,
+                        haptics: true,
+                        infiniteLoop: true,
+                        itemWidth: 100,
+                        itemHeight: 30,
+                        onChanged: (value) => setState(() => _minutes = value),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 90,
+                  height: 100,
+                  child: Center(
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.orange[600],
+                      child: NumberPicker(
+                        value: _hour,
+                        textStyle: TextStyle(fontSize: 25, color: Colors.white),
+                        selectedTextStyle: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                        minValue: 1,
+                        maxValue: 12,
+                        haptics: true,
+                        infiniteLoop: true,
+                        itemWidth: 100,
+                        itemHeight: 30,
+                        onChanged: (value) => setState(() => _hour = value),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
