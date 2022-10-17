e01618f5d163af07c2651766db1d600e7684280b
TonnyKamau <tonnykamau6@gmail.com>
2022-10-17 14:05:53 +0300

    new design

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -12,79 +12,23 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       
+      drawer: Drawer(),
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
-            
+            backgroundColor: Colors.transparent,
+            leading: IconButton(
+              icon: const Icon(
+                Icons.notes_rounded,
+              ),
+              iconSize: 24.0,
+              onPressed: () => Scaffold.of(context).openDrawer(),
+            ),
             floating: true,
-            snap: true,
-            title: _bar(context),
-            automaticallyImplyLeading: false,
-    centerTitle: true,
-    titleSpacing: 0,
-    backgroundColor: Colors.transparent,
-    elevation: 0,
-          ),
-          const SliverToBoxAdapter(
-            child:SizedBox(
-              height:24,
-            )
-          ),
-
-          const SliverToBoxAdapter(
-            child:SizedBox(
-              height:80,
-            )
+            elevation: 0,
           ),
         ],
       ),
     );
   }
-
-  Widget _bar(BuildContext context) => Container(
-        decoration:BoxDecoration(
-          borderRadius:BorderRadius.circular(24),
-        ),
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Card(
-          elevation: 2,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: Row(children: <Widget>[
-              const SizedBox(
-                width: 20,
-              ),
-            
-     InkWell(
-              child: const Icon(Icons.menu),
-              onTap: () => Scaffold.of(context).openDrawer(),
-            ),
-              Expanded(
-                  child: Center(
-                    child: Text(
-                'Search your notes',
-                softWrap: false,
-              ),
-                  )),
-              InkWell(
-                  child: Icon(isGrid ? Icons.view_list : Icons.view_module),
-                  onTap: () => setState(() {
-                        isGrid = !isGrid;
-                      })),
-              const SizedBox(
-                width: 18,
-              ),
-              CircleAvatar(
-                child: Icon(Icons.face),
-                radius: 17,
-              ),
-              const SizedBox(
-                width: 10,
-              )
-            ]),
-          ),
-        ),
-      );
 }
